626f5887d01a54117dddec19c5e9d5197a27fdfb
Muhammad jasim <jasimjamilboat@gmail.com>
2025-02-03 20:31:03 +0500

    web new

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add any interactive animations or functionality here
+// Example: Smooth scrolling for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
